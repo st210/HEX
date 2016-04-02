fca6ef66f06465f3335275235da0c9cf11e71844
Verdimini <verdimini@yahoo.fr>
2016-04-02 18:53:52 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,83 +1,49 @@
-#ifdef __cplusplus
-    #include <cstdlib>
-#else
-    #include <stdlib.h>
-#endif
+#include "Jeu.h"
+#include "menu.h"
 
 #include <stdio.h>
-#include <SDL.h>
-#include <SDL_image.h>
+#include <stdlib.h>
+
+#include <SDL/SDL.h>
+#include <SDL/SDL_keysym.h>
+#include "SDL_image.h"
+#include "SDL_ttf.h"
 
 
-int main(int argc, char *argv[])
+int main (int argc, char *argv[])
 {
-    // VARIABLES
-    SDL_Surface *ecran = NULL, *pion = NULL;
-    SDL_Rect position_pion;
+    SDL_Surface *ecran = NULL;
+    Uint8 video_bpp = 32;
+    int continuer, choix;
     SDL_Event event;
-    int fin = 0;
 
+    continuer = 1;
 
-    // INITIALIZATION
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_WM_SetCaption(" HEX: Take it easy !", NULL);
-    SDL_WM_SetIcon(IMG_Load("images/hex_icon.png"), NULL);
 
+    if (SDL_Init(SDL_INIT_VIDEO) == -1)
+        {
+            fprintf(stderr, "Erreur d'initialisation de la SDL : %s\n", SDL_GetError());
+            exit(EXIT_FAILURE);
+        }
+    TTF_Init();
+    ecran = SDL_SetVideoMode(WINWI, WINHI, video_bpp, SDL_HWSURFACE | SDL_DOUBLEBUF);
 
-    // OPEN SCREEN
-    ecran = SDL_SetVideoMode(800, 600, 32, SDL_HWSURFACE);
     if (ecran == NULL)
     {
         fprintf(stderr, "Impossible de charger le mode vidéo : %s\n", SDL_GetError());
         exit(EXIT_FAILURE);
     }
 
-    // UPDATE SCREEN
-    // background
-    SDL_FillRect(ecran, NULL, SDL_MapRGB(ecran->format, 255, 255, 255));
-    // pion
-    pion = IMG_Load("images/button_blue_mini.png");
+    SDL_WM_SetCaption("Jeu de HEX", NULL);
+    SDL_WM_SetIcon(IMG_Load("Images/hex_icon.png"), NULL);
 
-    // ACTIONS
-    while (!fin)
-    {
-        SDL_WaitEvent(&event);
-        switch(event.type)
-        {
-            // close window
-            case SDL_QUIT:
-                fin = 1;
-                break;
-            // key pressed
-            case SDL_KEYDOWN:
-                switch (event.key.keysym.sym)
-                {
-                    case SDLK_ESCAPE:
-                        fin = 1;
-                        break;
-                }
-                break;
-            // mouse click
-            case SDL_MOUSEBUTTONUP:
-                if (event.button.button == SDL_BUTTON_RIGHT)
-                    fin = 1;
-                if (event.button.button == SDL_BUTTON_LEFT)
-                {
-                    position_pion.x = event.button.x;
-                    position_pion.y = event.button.y;
-                }
-                break;
-        }
-
-        SDL_SetColorKey(pion, SDL_SRCCOLORKEY, SDL_MapRGB(pion->format, 255, 255, 255)); // transparency
-        SDL_BlitSurface(pion, NULL, ecran, &position_pion);
-        SDL_Flip(ecran);
-    }
+    choix = create_menu(ecran);
+    jeu(ecran);
 
 
-    // END
-    // free memory space
-    SDL_FreeSurface(pion);
+    TTF_Quit();
+    SDL_FreeSurface(ecran);
     SDL_Quit();
 
     return EXIT_SUCCESS;
