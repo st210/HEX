5cc5c97255265ebc061bedc147ddd47b14eefec6
Verdimini <verdimini@yahoo.fr>
2016-04-02 18:52:30 +0200

    Create Jeu.h

---
+++ Jeu.h
@@ -0,0 +1,11 @@
+#ifndef JEU_H_INCLUDED
+#define JEU_H_INCLUDED
+
+#include <SDL/SDL.h>
+#include <SDL/SDL_keysym.h>
+#include "SDL_image.h"
+#include "SDL_ttf.h"
+
+void jeu(SDL_Surface *ecran);
+
+#endif // JEU_H_INCLUDED
